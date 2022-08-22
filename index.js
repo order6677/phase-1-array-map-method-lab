682f43ea3e7d0e1e38421efff4be285483ea8abb
awadh <awadh.hassan@student.moringaschool.com>
2022-08-22 09:05:30 +0300

    assignment done

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+  return tutorials.map( el => {
+       el = el.split(" ")
+       for (let i =0; i < el.length; i++){
+           el[i] = el[i][0].toUpperCase() + el[i].slice(1)
+       }
+       return el.join(" ")
+   })
 }
